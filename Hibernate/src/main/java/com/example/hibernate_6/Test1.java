9de4ae7036a700489b7e7dda513b53a331339dbe
Dimoonevs <d12k13v15b@gmail.com>
2023-08-30 23:54:38 +0200

    Hibernate ManyToMany

--- Hibernate/src/main/java/com/example/hibernate_6/Test1.java
+++ Hibernate/src/main/java/com/example/hibernate_6/Test1.java
@@ -1,5 +1,7 @@
 package com.example.hibernate_6;
 
+import com.example.hibernate_6.entity.Child;
+import com.example.hibernate_6.entity.Section;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -8,6 +10,8 @@ public class Test1 {
     public static void main(String[] args) {
         SessionFactory factory =
                 new Configuration().configure("hibernate.cfg.xml")
+                        .addAnnotatedClass(Child.class)
+                        .addAnnotatedClass(Section.class)
                         .buildSessionFactory();
 
 
@@ -15,8 +19,109 @@ public class Test1 {
         try {
             session = factory.getCurrentSession();
             session.beginTransaction();
+//            Section section1 = new Section("MMA");
+//            Section section2 = new Section("Football");
+//            Child child1 = new Child("Dima", 17);
+//            Child child2 = new Child("Ivan", 13);
+//            Child child3 = new Child("Stanislav", 17);
+//
+//            section1.addChildToSection(child1);
+//            section1.addChildToSection(child3);
+//            section2.addChildToSection(child2);
+//            section2.addChildToSection(child3);
+//
+//            session.save(section1);
+//            session.save(section2);
+
+//            ***************************************
+
+//            Section sectionChess = new Section("Chess");
+//            Section sectionMMA = session.get(Section.class, 1);
+//            Section sectionFootball = session.get(Section.class, 2);
+//            Child child = new Child("Igor", 7);
+//            Child child1 = new Child("Masha", 12);
+//            Child child2 = new Child("Anton", 8);
+//            sectionChess.addChildToSection(child);
+//            sectionChess.addChildToSection(child1);
+//            sectionChess.addChildToSection(child2);
+//            sectionMMA.addChildToSection(child2);
+//            sectionFootball.addChildToSection(child);
+//
+//            session.save(sectionChess);
+//            session.save(sectionMMA);
+//            session.save(sectionFootball);
+
+//            ***************************************
+
+//            Section section = session.get(Section.class, 1);
+//
+//            System.out.println(section);
+//            System.out.println(section.getChildren());
+
+//            ***************************************
+
+//            Child child = session.get(Child.class, 4);
+//
+//            System.out.println(child);
+//            System.out.println(child.getSections());
+
+//            ***************************************
+
+//            Section section = session.get(Section.class, 2);
+//
+//            session.delete(section);
+
+//            *************************************** After removing the CascadeType.REMOVE (CascadeType.ALL)
+
+//            Section sectionMMA = new Section("MMA");
+//            Section sectionFootball = new Section("Football");
+//            Section sectionChess = new Section("Chess");
+//            Section sectionMath = new Section("Math");
+//
+//            Child child1 = new Child("Dima", 17);
+//            Child child2 = new Child("Igor", 8);
+//            Child child3 = new Child("Ivan", 9);
+//            Child child4 = new Child("Stanislav", 16);
+//            Child child5 = new Child("Danil", 16);
+//            Child child6 = new Child("Maria", 17);
+//
+//            sectionMMA.addChildToSection(child1);
+//            sectionMMA.addChildToSection(child4);
+//            sectionMMA.addChildToSection(child5);
+//
+//            sectionMath.addChildToSection(child1);
+//            sectionMath.addChildToSection(child6);
+//            sectionMath.addChildToSection(child3);
+//            sectionMath.addChildToSection(child4);
+//
+//            sectionChess.addChildToSection(child1);
+//            sectionChess.addChildToSection(child2);
+//            sectionChess.addChildToSection(child4);
+//            sectionChess.addChildToSection(child6);
+//
+//            sectionFootball.addChildToSection(child2);
+//            sectionFootball.addChildToSection(child3);
+//
+//            session.persist(sectionMMA);
+//            session.persist(sectionChess);
+//            session.persist(sectionMath);
+//            session.persist(sectionFootball);
+
+//            ***************************************
+
+//            Section section = session.get(Section.class, 4);
+//
+//            session.delete(section);
+
+//            ***************************************
+
+            Child child = session.get(Child.class, 5);
+
+            session.delete(child);
+
 
             session.getTransaction().commit();
+            System.out.println("DONE!!!");
         }
         finally {
             session.close();
---
+++ Hibernate/src/main/java/com/example/hibernate_6/entity/Child.java
@@ -0,0 +1,80 @@
+package com.example.hibernate_6.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "children")
+public class Child {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private Integer age;
+    @ManyToMany(cascade = {CascadeType.MERGE,CascadeType.PERSIST, CascadeType.DETACH,CascadeType.REFRESH})
+    @JoinTable(
+            name = "child_section",
+            joinColumns = @JoinColumn(name = "child_id"),
+            inverseJoinColumns = @JoinColumn(name = "section_id")
+    )
+    private List<Section> sections;
+
+    public Child() {
+    }
+
+    public Child(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public List<Section> getSections() {
+        return sections;
+    }
+
+    public void setSections(List<Section> sections) {
+        this.sections = sections;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void  addSectionToChild(Section section){
+            if (sections == null){
+                sections = new ArrayList<>();
+            }
+            sections.add(section);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Child{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
